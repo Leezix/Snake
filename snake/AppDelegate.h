95d2fb10d989bb8dda101244223a2604dc4d9851
李主贤 <lizhuxian020@pingan.com.cn>
2017-03-30 22:33:48 +0800

    新建工程

---
+++ snake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  snake
+//
+//  Created by lzx on 17/3/29.
+//  Copyright © 2017年 lzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ snake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  snake
+//
+//  Created by lzx on 17/3/29.
+//  Copyright © 2017年 lzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
